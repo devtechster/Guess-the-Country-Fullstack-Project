ffd3141ad681093c53eeb4af76f63111a5e82651
Adhiraj <adhirajbudukh@gmail.com>
2025-06-25 13:09:07 -0400

    gh pages comit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: 'https://github.com/devtechster/Guess-the-Country-Fullstack-Project.git',  // 🔴 Important!
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
